1e2846bdabcccac116ababd92ff6903fbc78413b
Fábio Miranda <fabioviannatj@gmail.com>
2018-03-23 18:40:03 -0300

    feat: :sparkles: finish implementation the form and results

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { PlansService } from './../services/plans.service';
+import { FormGroup, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
-
+import { DestinationsService } from './../services/destinations.service';
+import { PlansService } from './../services/plans.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,19 +12,64 @@ import { Observable } from 'rxjs/Observable';
 
 export class HomeComponent implements OnInit {
 
+  result: { minutes: any; localeOrigin: any; localeDestination: any; valuePlan: number; valueNoPlan: number; };
+  origens: any;
+  destinations: any;
   plans: any;
-  constructor(private plansService: PlansService) {
+  calculatorFaleMaisForm: FormGroup;
+  showResults: boolean;
+  constructor(private plansService: PlansService,
+    private destinationsService: DestinationsService,
+    private fb: FormBuilder, ) {
+
+    this.calculatorFaleMaisForm = this.fb.group({
+      'origem': [null, Validators.required],
+      'destino': [null, Validators.required],
+      'minutos': [null, Validators.required],
+      'plano': [null, Validators.required],
+
+    });
+    this.getInitialFormData();
+  }
+
+  ngOnInit() {
+    this.calculatorFaleMaisForm.controls['origem'].statusChanges.subscribe(res => {
+      this.destinations = this.calculatorFaleMaisForm.controls['origem'].value;
+      this.destinations = this.destinations.destinos;
+      this.calculatorFaleMaisForm.controls['destino'].enable();
+    });
+    this.calculatorFaleMaisForm.controls['destino'].disable();
+    this.showResults = false;
+  }
+
+  getInitialFormData() {
     this.plansService.getAllPlans()
       .subscribe(plans => {
         this.plans = plans;
       }, err => {
-
+        console.error(err);
       });
 
-}
+    this.destinationsService.getAllDestinations()
+      .subscribe(origens => {
+        this.origens = origens;
+      });
 
-  ngOnInit() {
   }
 
+  calculateResult() {
+    let data = {
+      origin: this.calculatorFaleMaisForm.controls['origem'].value,
+      destinationSelected: this.calculatorFaleMaisForm.controls['destino'].value,
+      minutos: this.calculatorFaleMaisForm.controls['minutos'].value,
+      plan: this.calculatorFaleMaisForm.controls['plano'].value
+    };
+    this.result = this.plansService.calculatePlansResult(data);
+    this.showResults = true;
+    this.calculatorFaleMaisForm.controls['origem'].setValue('');
+    this.calculatorFaleMaisForm.controls['destino'].setValue('');
+    this.calculatorFaleMaisForm.controls['minutos'].setValue(0);
+    this.calculatorFaleMaisForm.controls['plano'].setValue('');
+  }
 
 }
--- src/app/services/plans.service.ts
+++ src/app/services/plans.service.ts
@@ -14,4 +14,26 @@ export class PlansService {
             });
 
     }
+
+    calculatePlansResult(dataCall: any) {
+        let valueNoPlan = (dataCall.destinationSelected.tarifa * dataCall.minutos);
+        let valuePlan;
+
+
+        if (dataCall.plan.minutos >= dataCall.minutos) {
+            valuePlan = 0;
+        } else {
+            valuePlan = ((dataCall.minutos - dataCall.plan.minutos) * (dataCall.destinationSelected.tarifa * 1.1));
+        }
+
+        let data = {
+            minutes: dataCall.minutos,
+            localeOrigin: dataCall.origin.ddd,
+            localeDestination: dataCall.destinationSelected.destino,
+            valuePlan: valuePlan,
+            valueNoPlan: valueNoPlan
+        };
+
+        return data;
+    }
 }
